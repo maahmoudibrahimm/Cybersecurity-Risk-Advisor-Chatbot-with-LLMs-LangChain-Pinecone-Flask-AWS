d34315ea002ef738ff38e6e9e37a4202e236aa90
Mahmoud Ibrahim <mahmoud.fcb10@gmail.com>
2026-05-06 17:38:20 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cybersecurity_chatbot",
+    version="0.1.0",
+    author="Mahmoud Ibrahim",
+    author_email="mahmoud.fcb10@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
